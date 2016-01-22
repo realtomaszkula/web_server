93283425607902d30aa669ead1e358ffd0536431
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-22 15:31:45 +0100

    improved headers

--- server.rb
+++ server.rb
@@ -5,24 +5,25 @@ server = TCPServer.open(2000)
 loop {
   client = server.accept
 
-  request = client.read.split
+
+  puts  "#{request = client.read.split}"
 
   ## status line
   status_code = "200 OK"
-  status_line = "HTTP/1.1 #{status_code}"
+  status_line = "HTTP/1.1 #{status_code}\n"
 
   ## headers
-  date = Time.now.ctime
-  content_type = "text/html"
-  content_length = 1
-  headers = date + content_type + content_length
+  date = "Date: #{Time.now.ctime}"
+  content_type = "Content-Type: text/html"
+  content_length = "Content-Length: 123"
+  headers = date + "\n" + content_type + "\n" + content_length.to_s + "\n\n"
 
   # body
   name = "index.html"
   body = File.read(name)
 
   # full server response
-  response = status_line + headers + body
+  puts "#{response = status_line + headers + body}"
 
   client.puts response
   client.close
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -7,8 +7,8 @@ path = "index.html"
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
 socket = TCPSocket.open(host,port)
-socket.print(request)
-puts socket.gets
+socket.print(request).read
+puts socket
 
 # headers,body = response.split("\r\n\r\n", 2)
 # print body
