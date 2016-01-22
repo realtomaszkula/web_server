91039683ad47433ee1a6581d538f6f5fde655f63
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-22 15:23:12 +0100

    started working on server response

--- server.rb
+++ server.rb
@@ -4,7 +4,26 @@ server = TCPServer.open(2000)
 
 loop {
   client = server.accept
-  client.puts(Time.now.ctime)
-  client.puts "Closing the connection. Bye!"
+
+  request = client.read.split
+
+  ## status line
+  status_code = "200 OK"
+  status_line = "HTTP/1.1 #{status_code}"
+
+  ## headers
+  date = Time.now.ctime
+  content_type = "text/html"
+  content_length = 1
+  headers = date + content_type + content_length
+
+  # body
+  name = "index.html"
+  body = File.read(name)
+
+  # full server response
+  response = status_line + headers + body
+
+  client.puts response
   client.close
 }
